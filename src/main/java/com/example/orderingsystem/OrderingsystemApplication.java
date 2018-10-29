d6b3ab3aea29756e199e2223a441066b7ee3d877
happyOwen <43246452+happyOwen@users.noreply.github.com>
2018-10-29 23:03:53 +0800

    first commit

---
+++ src/main/java/com/example/orderingsystem/OrderingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.orderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingsystemApplication.class, args);
+	}
+}
